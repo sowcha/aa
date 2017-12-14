21971d1f98c05049ee448a7e82cc01e4df52f9b0
sowcha <32437738+sowcha@users.noreply.github.com>
2017-12-14 20:34:41 +0530

    Create vorco.java

---
+++ vorco.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+class vorco
+{
+public tatic void main(String args[])
+{
+ char x1[]={'a','e','i','o','u','A','E','I','O','U'};
+ int y1;
+Scanner in=new scanner(system.in);
+y1=in.nextLine();
+void check(char[] ele, char y1)
+{
+for(y1:ele)
+{
+  if(y1==ele)
+System.out.println("vowel");
+  else
+System.out.println("consonant");
+ }
+}
+}
+}
